1d69772c0fc35ae094b745e112da6f8cd29cb18e
sunhuafu <sunhuafu@mafengwo.com>
2017-12-25 23:04:10 +0800

    revert 版本

--- rollback.php
+++ rollback.php
@@ -5,23 +5,3 @@ function helloworld(){
 	echo "hello world";
 }
 
-
-function huafufunc(){
-	echo "this branch is sunhuafu";
-}
-
-function huafufunc2(){
-	echo "this branch is sunhuafu and commit second";
-}
-
-function hhuafufunc3(){
-	echo "this branch is sunhuafu and commit third";
-}
-
-function otherfunc(){
-	echo "this branch is other1";
-}
-
-function other2func(){
-	echo "this branch is other2";
-}
