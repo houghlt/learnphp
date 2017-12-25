a77727aa1c2e3a38cb77a8fe13c3064785187548
sunhuafu <sunhuafu@mafengwo.com>
2017-12-25 22:37:10 +0800

    other1 提交

--- rollback.php
+++ rollback.php
@@ -5,3 +5,19 @@ function helloworld(){
 	echo "hello world";
 }
 
+
+function huafufunc(){
+	echo "this branch is sunhuafu";
+}
+
+function huafufunc2(){
+	echo "this branch is sunhuafu and commit second";
+}
+
+function hhuafufunc3(){
+	echo "this branch is sunhuafu and commit third";
+}
+
+function otherfunc(){
+	echo "this branch is other1";
+}
