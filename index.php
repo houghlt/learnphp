f43719e085777b47238fe7b72e0e26bf685a161f
sunhuafu <sunhuafu@mafengwo.com>
2017-12-26 16:03:58 +0800

    解决冲突

--- index.php
+++ index.php
@@ -5,10 +5,4 @@
 echo "hello world";
 echo "hello world 1";
 echo "hello world 2";
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of d64fb0d... 默认1，2
-echo "hello world 3";
-echo "hello world 4";
-echo "hello world 5";
